e557d1dc88ad1d9c1871a254f6cb9abd9916421f
ithiel883 <ajuka.zephiniah@gmail.com>
2022-07-25 17:31:54 +0100

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+  i am in the navbar
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './styles/app.css'; // Here
+import App from './App.vue'
+
+createApp(App).mount('#app')
